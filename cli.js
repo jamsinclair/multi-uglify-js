ac10dc856833836367a87ba9b4d06e18c2cfea66
jamsinclair <jamsinclairnz@gmail.com>
2021-07-18 21:01:46 +0900

    docs: fix spacing

--- cli.js
+++ cli.js
@@ -3,72 +3,75 @@ import meow from 'meow';
 import minify from './lib/minify.js';
 
 const cli = meow(`
-	Usage
-	  $ multi-uglifyjs [file globs]
+  Usage
+    $ multi-uglifyjs [file globs]
 
-	Options
-		--help                      Print usage information.
-		--version                   Print version of multi-uglifyjs lib.
+  Options
+    --help                      Print usage information.
+    --version                   Print version of multi-uglifyjs lib.
 
-		-p, --parse <options>       Specify parse options:
-																https://github.com/mishoo/UglifyJS#parse-options
+    -p, --parse <options>       Specify parse options:
+                                https://github.com/mishoo/UglifyJS#parse-options
 
-		-c, --compress [options]    Enable compressor/specify compressor options.
-																Options are comma separated and in the form of: "toplevel,sequences=false"
-																(The former value is defaulting "toplevel" to a true value.)
-																https://github.com/mishoo/UglifyJS#compress-options
+    -c, --compress [options]    Enable compressor/specify compressor options.
+                                Options are comma separated and in the form of: "toplevel,sequences=false"
+                                (The former value is defaulting "toplevel" to a true value.)
+                                https://github.com/mishoo/UglifyJS#compress-options
 
-		-m, --mangle [options]    	Mangle names/specify mangler options.
-																Options are comma separated and in the form of: "eval,reserved=['$','require','exports']"
-																(The former value is defaulting "eval" to a true value.)
-																https://github.com/mishoo/UglifyJS#mangle-options
+    -m, --mangle [options]    	Mangle names/specify mangler options.
+                                Options are comma separated and in the form of: "eval,reserved=['$','require','exports']"
+                                (The former value is defaulting "eval" to a true value.)
+                                https://github.com/mishoo/UglifyJS#mangle-options
 
-		--mangle-props [options]    Mangle properties/specify mangler options.
-																Options are comma separated and in the form of: "builtins,reserved=['doNotChange']"
+    --mangle-props [options]    Mangle properties/specify mangler options.
+                                Options are comma separated and in the form of: "builtins,reserved=['doNotChange']"
                                 https://github.com/mishoo/UglifyJS#mangle-properties-options
 
-		-O, --output-opts [options] Specify output options
+    -O, --output-opts [options] Specify output options
 
-		-o, --output <directory>    Output file directory
+    -o, --output <directory>    Output file directory
 
-		--annotations               Process and preserve comment annotations.
+    --annotations               Process and preserve comment annotations.
                                 (/*@__PURE__*/ or /*#__PURE__*/)
 
-		--ie                        Support non-standard Internet Explorer.
+    --ie                        Support non-standard Internet Explorer.
                                 Equivalent to setting 'ie: true' in 'minify()'
                                 for 'compress', 'mangle' and 'output' options.
                                 By default UglifyJS will not try to be IE-proof.
 
-		--keep-fnames               Do not mangle/drop function names.  Useful for
+    --keep-fnames               Do not mangle/drop function names.  Useful for
                                 code relying on Function.prototype.name.
 
-		--source-map [options]      Enable source map/specify source map options
-																https://github.com/mishoo/UglifyJS#source-map-options
+    --source-map [options]      Enable source map/specify source map options
+                                https://github.com/mishoo/UglifyJS#source-map-options
 
-		--toplevel                  Compress and/or mangle variables in top level scope.
+    --toplevel                  Compress and/or mangle variables in top level scope.
 
-		--v8                        Support non-standard Chrome & Node.js
-																Equivalent to setting 'v8: true' in 'minify()'
-																for 'mangle' and 'output' options.
-																By default UglifyJS will not try to be v8-proof.
+    --v8                        Support non-standard Chrome & Node.js
+                                Equivalent to setting 'v8: true' in 'minify()'
+                                for 'mangle' and 'output' options.
+                                By default UglifyJS will not try to be v8-proof.
 
-		--verbose                   Print diagnostic messages.
+    --verbose                   Print diagnostic messages.
 
-		--warn                      Print warning messages.
+    --warn                      Print warning messages.
 
-		--webkit                    Support non-standard Safari/Webkit.
-																Equivalent to setting 'webkit: true' in 'minify()'
-																for 'mangle' and 'output' options.
-																By default UglifyJS will not try to be Safari-proof.
+    --webkit                    Support non-standard Safari/Webkit.
+                                Equivalent to setting 'webkit: true' in 'minify()'
+                                for 'mangle' and 'output' options.
+                                By default UglifyJS will not try to be Safari-proof.
 
-	Examples
-		# Minify all JavaScript files in the assets directory
-		# and output minified files in place
-	  $ multi-uglifyjs assets/*.js
+  Examples
+    # Minify all JavaScript files in the assets directory
+    # and output minified files in place
+    $ multi-uglifyjs assets/*.js
 
-		# Minify all JavaScript files in the assets directory
-		# and output minified files in the dist
-	  $ multi-uglifyjs assets/*.js -o dist
+    # Minify all JavaScript files in the assets directory
+    # and output minified files in the dist
+    $ multi-uglifyjs assets/*.js -o dist
+
+    # Minify with a different file suffix "-min", becoming "-min.js"
+    $ multi-uglifyjs assets/*.js --suffix=-min
 `, {
 	importMeta: import.meta,
 	flags: {
